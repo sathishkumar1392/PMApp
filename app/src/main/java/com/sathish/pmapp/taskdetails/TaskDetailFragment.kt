80ccc0b340c0fb5479d75b6688fcc9f255574610
Sathish Kumar <sathish.engineer1@gmail.com>
2021-04-13 20:57:44 +0200

    Removed unused import

--- app/src/main/java/com/sathish/pmapp/taskdetails/TaskDetailFragment.kt
+++ app/src/main/java/com/sathish/pmapp/taskdetails/TaskDetailFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
-import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import com.sathish.pmapp.R
 import com.sathish.pmapp.database.entity.Comment
